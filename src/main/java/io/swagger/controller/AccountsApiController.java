f269080aa15770da1af2f8103e48d40ec64740dc
suzannekoopman <suusk2202@gmail.com>
2020-05-14 14:29:43 +0200

    account --> accounts

--- src/main/java/io/swagger/controller/AccountsApiController.java
+++ src/main/java/io/swagger/controller/AccountsApiController.java
@@ -11,6 +11,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
@@ -41,11 +43,11 @@ public class AccountsApiController implements AccountsApi {
     }
 
     public ResponseEntity<List<Account>> getAllAccounts(@ApiParam(value = "The number of items to skip before starting to collect the result set") @Valid @RequestParam(value = "offset", required = false) Integer offset
-,@ApiParam(value = "The numbers of items to return") @Valid @RequestParam(value = "limit", required = false) Integer limit
-,@ApiParam(value = "The user id of user that should own the account") @Valid @RequestParam(value = "userId", required = false) Integer userId
-,@ApiParam(value = "The iban the account should have") @Valid @RequestParam(value = "iban", required = false) String iban
-,@ApiParam(value = "The iban the account should have available accounts in the system", allowableValues = "current, savings") @Valid @RequestParam(value = "account_type", required = false) String accountType
-) {
+            ,@ApiParam(value = "The numbers of items to return") @Valid @RequestParam(value = "limit", required = false) Integer limit
+            ,@ApiParam(value = "The user id of user that should own the account") @Valid @RequestParam(value = "userId", required = false) Integer userId
+            ,@ApiParam(value = "The iban the account should have") @Valid @RequestParam(value = "iban", required = false) String iban
+            ,@ApiParam(value = "The iban the account should have available accounts in the system", allowableValues = "current, savings") @Valid @RequestParam(value = "account_type", required = false) String accountType
+    ) {
         String accept = request.getHeader("Accept");
 
         String apiKeyAuth = request.getHeader("ApiKeyAuth");
@@ -64,4 +66,60 @@ public class AccountsApiController implements AccountsApi {
         return new ResponseEntity<List<Account>>(accountService.getAllAccounts(), HttpStatus.OK);
     }
 
+    public ResponseEntity<Void> deleteAccountByIban(@ApiParam(value = "iban of a specific account",required=true) @PathVariable("iban") String iban
+    ) {
+        String accept = request.getHeader("Accept");
+
+        String apiKeyAuth = request.getHeader("ApiKeyAuth");
+        if(!authService.IsUserAuthenticated(apiKeyAuth, 0))
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+
+        return new ResponseEntity<Void>(HttpStatus.valueOf(accountService.deleteAccount(iban)));
+    }
+
+    public ResponseEntity<Account> getSpecificAccount(@ApiParam(value = "user of a specific account",required=true) @PathVariable("iban") String iban
+    ) {
+        String accept = request.getHeader("Accept");
+
+        String apiKeyAuth = request.getHeader("ApiKeyAuth");
+        if(!authService.IsUserAuthenticated(apiKeyAuth, 0))
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"owner\" : 1,\n  \"account_type\" : [ \"current\", \"current\" ],\n  \"transactionDayLimit\" : 100,\n  \"balance\" : 200,\n  \"transactionAmountLimit\" : 200,\n  \"iban\" : \"NL11INHO0123456789\",\n  \"balanceLimit\" : -1200\n}", Account.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        return new ResponseEntity<Account>(accountService.getSpecificAccount(iban), HttpStatus.OK);
+    }
+
+    public ResponseEntity<Account> updateSpecificAccount(@ApiParam(value = "" ,required=true )  @Valid @RequestBody Account body
+            ,@ApiParam(value = "user of a specific account",required=true) @PathVariable("iban") String iban
+    ) {
+        String accept = request.getHeader("Accept");
+
+        String apiKeyAuth = request.getHeader("ApiKeyAuth");
+        if(!authService.IsUserAuthenticated(apiKeyAuth, 0))
+            return new ResponseEntity(HttpStatus.FORBIDDEN);
+//        try {
+//            return new ResponseEntity<Account>(HttpStatus.valueOf(accountService.updateAccount(body)));
+//        }
+//        catch (Exception e) {
+//            System.out.println(e);
+//            return new ResponseEntity<Account>(HttpStatus.NOT_ACCEPTABLE);
+//        }
+        if (accept != null && accept.contains("application/json")) {
+            try {
+                return new ResponseEntity<Account>(objectMapper.readValue("{\n  \"owner\" : 1,\n  \"account_type\" : [ \"current\", \"current\" ],\n  \"transactionDayLimit\" : 100,\n  \"balance\" : 200,\n  \"transactionAmountLimit\" : 200,\n  \"iban\" : \"NL11INHO0123456789\",\n  \"balanceLimit\" : -1200\n}", Account.class), HttpStatus.NOT_IMPLEMENTED);
+            } catch (IOException e) {
+                log.error("Couldn't serialize response for content type application/json", e);
+                return new ResponseEntity<Account>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        return new ResponseEntity<Account>(HttpStatus.NOT_IMPLEMENTED);
+    }
 }
